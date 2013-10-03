2651f8ea063920f82146f602efb9325ccbac4bce
CptSpaceToaster <Lugia978@yahoo.com>
2013-10-02 22:36:01 -0400

    Cleaned up some redundant assets

--- yamhaven/easycoloredglass/lib/ModInfo.java
+++ yamhaven/easycoloredglass/lib/ModInfo.java
@@ -4,7 +4,7 @@ public class ModInfo {
 
 	public static final String ID = "EasyColoredGlass";
 	public static final String NAME = "Easy Colored Glass";
-	public static final String VERSION = "1.6.2.4";
+	public static final String VERSION = "1.6.4.100";
 	public static final String CHANNEL = ID;
 	public static final String PROXY_LOCATION = "yamhaven.easycoloredglass.proxy";
 	
