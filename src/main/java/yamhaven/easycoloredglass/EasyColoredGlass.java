64adb0b38fb731636dbc0ed3a88a4f12e754f336
CptSpaceToaster <CptSpaceToaster@gmail.com>
2015-02-08 15:08:16 -0500

    formatting

--- src/main/java/yamhaven/easycoloredglass/EasyColoredGlass.java
+++ src/main/java/yamhaven/easycoloredglass/EasyColoredGlass.java
@@ -20,8 +20,6 @@ import cpw.mods.fml.common.Mod.EventHandler;
 @Mod(modid = EasyColoredGlass.MOD_ID, version = EasyColoredGlass.VERSION)
 public class EasyColoredGlass
 {
-
-
     public static final String MOD_ID = "easycoloredglass";
     public static final String MOD_NAME = "Easy Colored Glass";
     public static final String VERSION = "1.0.0";
